4e8078b5e166822c3b5b41909456fa56dda5e01e
claudiosmweb <contato@claudiosmweb.com>
2014-05-31 13:50:01 -0300

    created initial plugin file

---
+++ woocommerce-pagarme.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Pagar.me
+ * Plugin URI: http://github.com/claudiosmweb/woocommerce-pagarme
+ * Description: Gateway de pagamento Pagar.me para WooCommerce.
+ * Author: Pagar.me, claudiosanches
+ * Author URI: https://pagar.me/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-pagarme
+ * Domain Path: /languages/
+ */
