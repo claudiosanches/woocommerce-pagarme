fe446767a73de85ae94030358413369896f1d4b9
claudiosmweb <contato@claudiosmweb.com>
2014-06-01 14:35:54 -0300

    created the WC_PagarMe class

--- woocommerce-pagarme.php
+++ woocommerce-pagarme.php
@@ -10,3 +10,100 @@
  * Text Domain: woocommerce-pagarme
  * Domain Path: /languages/
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'WC_PagarMe' ) ) :
+
+/**
+ * WooCommerce WC_PagarMe main class.
+ */
+class WC_PagarMe {
+
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin public actions.
+	 */
+	private function __construct() {
+		// Load plugin text domain
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+		// Checks with WooCommerce is installed.
+		if ( class_exists( 'WC_Payment_Gateway' ) ) {
+			// Include the WC_PagarMe_Gateway class.
+			// include_once 'includes/class-wc-pagarme-gateway.php';
+
+			// add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateway' ) );
+			add_filter( 'woocommerce_available_payment_gateways', array( $this, 'hides_when_is_outside_brazil' ) );
+		} else {
+			add_action( 'admin_notices', array( $this, 'woocommerce_missing_notice' ) );
+		}
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), 'woocommerce-pagarme' );
+
+		load_textdomain( 'woocommerce-pagarme', trailingslashit( WP_LANG_DIR ) . 'woocommerce-pagarme/woocommerce-pagarme-' . $locale . '.mo' );
+		load_plugin_textdomain( 'woocommerce-pagarme', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Add the gateway to WooCommerce.
+	 *
+	 * @param   array $methods WooCommerce payment methods.
+	 *
+	 * @return  array          Payment methods with Pagar.me.
+	 */
+	public function add_gateway( $methods ) {
+		$methods[] = 'WC_PagarMe_Gateway';
+
+		return $methods;
+	}
+
+	/**
+	 * WooCommerce fallback notice.
+	 *
+	 * @return  string
+	 */
+	public function woocommerce_missing_notice() {
+		echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Pagar.me Gateway depends on the last version of %s to work!', 'woocommerce-pagarme' ), '<a href="http://wordpress.org/extend/plugins/woocommerce/">' . __( 'WooCommerce', 'woocommerce-pagarme' ) . '</a>' ) . '</p></div>';
+	}
+}
+
+add_action( 'plugins_loaded', array( 'WC_PagarMe', 'get_instance' ), 0 );
+
+endif;
