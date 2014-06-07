5579886b69fc9b282b23ba315a31cce1b4ebc332
claudiosmweb <contato@claudiosmweb.com>
2014-06-07 19:04:55 -0300

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+	'use strict';
+
+	grunt.initConfig({
+
+		// Setting folder templates
+		dirs: {
+			css:    'assets/css',
+			fonts:  'assets/fonts',
+			images: 'assets/images',
+			js:     'assets/js'
+		},
+
+		// Javascript linting with jshint
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= dirs.js %>/*/*.js',
+				'!<%= dirs.js %>/*/*.min.js'
+			]
+		},
+
+		// Minify .js files.
+		uglify: {
+			options: {
+				preserveComments: 'some'
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: '<%= dirs.js %>/',
+					ext: '.min.js'
+				}]
+			}
+		},
+
+		// Watch changes for assets
+		watch: {
+			js: {
+				files: [
+					'<%= dirs.js %>/*js',
+					'!<%= dirs.js %>/*.min.js'
+				],
+				tasks: ['jshint', 'uglify']
+			}
+		},
+
+		// Image optimization
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 7,
+					progressive: true
+				},
+				files: [{
+					expand: true,
+					cwd: './',
+					src: 'screenshot-*.png',
+					dest: './'
+				}]
+			}
+		}
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'jshint',
+		'uglify'
+	]);
+};
