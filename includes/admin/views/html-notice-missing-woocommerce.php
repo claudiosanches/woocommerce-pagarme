4b0a5c50a1c1ce4bd80687140037a3d1db1d7c22
Claudio Sanches <contato@claudiosmweb.com>
2016-01-26 23:34:54 -0200

    Added missing woocommerce notice file

---
+++ includes/admin/views/html-notice-missing-woocommerce.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Missing WooCommerce notice.
+ *
+ * @package WooCommerce_Pagarme/Admin/Notices
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$is_installed = false;
+
+if ( function_exists( 'get_plugins' ) ) {
+	$all_plugins  = get_plugins();
+	$is_installed = ! empty( $all_plugins['woocommerce/woocommerce.php'] );
+}
+
+?>
+
+<div class="error">
+	<p><strong><?php esc_html_e( 'WooCommerce Pagar.me', 'woocommerce-pagarme' ); ?></strong> <?php esc_html_e( 'depends on the last version of WooCommerce to work!', 'woocommerce-pagarme' ); ?></p>
+
+	<?php if ( $is_installed && current_user_can( 'install_plugins' ) ) : ?>
+		<p><a href="<?php echo esc_url( wp_nonce_url( self_admin_url( 'plugins.php?action=activate&plugin=woocommerce/woocommerce.php&plugin_status=active' ), 'activate-plugin_woocommerce/woocommerce.php' ) ); ?>" class="button button-primary"><?php esc_html_e( 'Active WooCommerce', 'woocommerce-pagarme' ); ?></a></p>
+	<?php else :
+		if ( current_user_can( 'install_plugins' ) ) {
+			$url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=woocommerce' ), 'install-plugin_woocommerce' );
+		} else {
+			$url = 'http://wordpress.org/plugins/woocommerce/';
+		}
+	?>
+		<p><a href="<?php echo esc_url( $url ); ?>" class="button button-primary"><?php esc_html_e( 'Install WooCommerce', 'woocommerce-pagarme' ); ?></a></p>
+	<?php endif; ?>
+</div>
