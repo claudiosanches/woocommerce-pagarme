13d95b1ec17ab8affd058effc686e80aed46e372
Leonam Pereira Dias <leonam.pd@gmail.com>
2019-03-28 08:38:55 -0300

    setup: create company temporary
    Creates a company temporary to be used during development and tests
    workflows
    
    Co-authored-by: Marcia Lima <ahmarcia@users.noreply.github.com>
    Co-authored-by: Murilo Souza <murilohns@users.noreply.github.com>

--- bash.php
+++ bash.php
@@ -1,5 +1,67 @@
 <?php
 
+/**
+ * Update Woocommerce Pagar.me Options
+ *
+ * @param string $optionName Option name
+ * @param array $options Values to update
+ *
+ * @return string
+ */
+function setup($optionName, $options)
+{
+    $command = sprintf(
+        "wp option update %s '%s' --format=json --allow-root",
+        $optionName,
+        json_encode($options)
+    );
+
+    return shell_exec($command);
+}
+
+/**
+ * Creates and retrieve data from company temporary route
+ *
+ * @return stdClass
+ */
+function getCompanyTemporary()
+{
+    $ch = curl_init();
+    curl_setopt(
+        $ch,
+        CURLOPT_URL,
+        "https://api.pagar.me/1/companies/temporary"
+    );
+    date_default_timezone_set('America/Sao_Paulo');
+    $params = sprintf(
+        'name=acceptance_test_company&email=%s@woocoomercesuite.com&password=password',
+        date('YmdHis')
+    );
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt(
+        $ch,
+        CURLOPT_POSTFIELDS,
+        $params
+    );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    $companyData = json_decode($result);
+    curl_close($ch);
+
+    return $companyData;
+}
+$apiKey = getenv('API_KEY');
+$encryptionKey = getenv('ENCRYPTION_KEY');
+
+if (!$apiKey && !$encryptionKey) {
+    $companyData = getCompanyTemporary();
+    $apiKey = $companyData->api_key->test;
+    $encryptionKey = $companyData->encryption_key->test;
+}
+
+define('PAGARME_API_KEY', $apiKey);
+define('PAGARME_ENCRYPTION_KEY', $encryptionKey);
+
 define(
     'WCP_CREDITCARD_OPTION_NAME',
     'woocommerce_pagarme-credit-card_settings'
@@ -10,47 +72,30 @@ define(
     'woocommerce_pagarme-banking-ticket_settings'
 );
 
-/**
- * Update Woocommerce Pagar.me Options
- *
- * @param string $optionName
- * @param array $options
- */
-function setup($optionName, $options) {
-    $command = sprintf(
-        "wp option update %s '%s' --format=json --allow-root",
-        $optionName,
-        json_encode($options)
-    );
-    return shell_exec($command);
-}
-
-$woocommerceCreditCardOptions = [
+$baseOptions = [
     'enabled' => 'yes',
-    'title' => 'Cartão de crédito',
-    'description' => 'Cartão de crédito Pagar.me',
-    'integration' => '',
-    'api_key' => getenv('API_KEY'),
-    'encryption_key' => getenv('ENCRYPTION_KEY'),
-    'checkout' => 'yes',
-    'max_installment' => '12',
-    'smallest_installment' => '1',
-    'interest_rate' => '1',
-    'free_installments' => '0',
+    'api_key' => PAGARME_API_KEY,
+    'encryption_key' => PAGARME_ENCRYPTION_KEY,
     'testing' => 'yes',
     'debug' => 'yes'
 ];
 
-$woocommerceBoletoOptions = [
-    'enabled' => 'Boleto bancário',
-    'description' => 'Pagar com boleto bancário',
+$woocommerceCreditCardOptions = array_merge($baseOptions, [
+    'title' => 'Cartão de crédito',
+    'description' => 'Cartão de crédito Pagar.me',
     'integration' => '',
-    'api_key' => getenv('API_KEY'),
-    'encryption_key' => getenv('ENCRYPTION_KEY'),
+    'checkout' => 'no',
+    'max_installment' => '12',
+    'smallest_installment' => '1',
+    'interest_rate' => '5',
+    'free_installments' => '2',
+]);
+
+$woocommerceBoletoOptions = array_merge($baseOptions, [
+    'title' => 'Boleto bancário',
+    'description' => 'Pagar com boleto bancário',
     'async' => 'no',
-    'testing' => '',
-    'debug' => 'yes'
-];
+]);
 
 echo setup(WCP_CREDITCARD_OPTION_NAME, $woocommerceCreditCardOptions);
 echo setup(WCP_BOLETO_OPTION_NAME, $woocommerceBoletoOptions);
