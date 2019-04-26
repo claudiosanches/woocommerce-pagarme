pagarme/woocommerce-pagarme-archived

Fixed coding standards

assets/js/checkout.js
@@ -212,7 +212,7 @@
 				success: function( data ) {
 					formSubmit = true;
 
-					if( wcPagarmeParams.refused_order === "no" ) {
+					if ( 'no' === wcPagarmeParams.refused_order ) {
 						// Remove any old token input.
 						$( 'input[name=pagarme_checkout_token]', form ).remove();
 
@@ -239,7 +239,7 @@
 				customer = getCustomerFields( form );
 			}
 
-			var shouldCreateToken = wcPagarmeParams.refused_order === "yes"? "false" : "true";
+			var shouldCreateToken = 'yes' === wcPagarmeParams.refused_order ? 'false' : 'true';
 
 			// Set params.
 			params = $.extend({}, {

includes/class-wc-pagarme-api.php
@@ -661,10 +661,10 @@ class WC_Pagarme_API {
 	public function process_regular_payment( $order_id ) {
 		$order = wc_get_order( $order_id );
 
-		$isUsingPagarmeCheckout = (isset($this->gateway->checkout) && 'yes' === $this->gateway->checkout);
-		$shouldRegisterDeniedTransaction = (isset($this->gateway->refused_order) && 'yes' === $this->gateway->refused_order);
+		$is_checkout_pagarme = isset( $this->gateway->checkout ) && 'yes' === $this->gateway->checkout;
+		$refuse_order        = isset( $this->gateway->refused_order ) && 'yes' === $this->gateway->refused_order;
 
-		if ( $isUsingPagarmeCheckout && !$shouldRegisterDeniedTransaction ) {
+		if ( $is_checkout_pagarme && ! $refuse_order ) {
 			if ( ! empty( $_POST['pagarme_checkout_token'] ) ) {
 				$token = sanitize_text_field( wp_unslash( $_POST['pagarme_checkout_token'] ) );
 				$data  = $this->generate_checkout_data( $order, $token );

includes/class-wc-pagarme-credit-card-gateway.php
@@ -39,7 +39,7 @@ class WC_Pagarme_Credit_Card_Gateway extends WC_Payment_Gateway {
 		$this->api_key              = $this->get_option( 'api_key' );
 		$this->encryption_key       = $this->get_option( 'encryption_key' );
 		$this->checkout             = $this->get_option( 'checkout' );
-		$this->refused_order				= $this->get_option( 'refused_order' );
+		$this->refused_order        = $this->get_option( 'refused_order' );
 		$this->max_installment      = $this->get_option( 'max_installment' );
 		$this->smallest_installment = $this->get_option( 'smallest_installment' );
 		$this->interest_rate        = $this->get_option( 'interest_rate', '0' );
@@ -135,12 +135,12 @@ class WC_Pagarme_Credit_Card_Gateway extends WC_Payment_Gateway {
 				'description' => __( "When enabled opens a Pagar.me modal window to receive the customer's credit card information.", 'woocommerce-pagarme' ),
 			),
 			'refused_order' => array(
-				'title' => __( 'Refused Transactions Order', 'woocommerce-pagarme'),
-				'type'  => 'checkbox',
-				'label' => __( 'Create order for refused transactions', 'woocommerce-pagarme'),
-				'default' => 'no',
-				'desc_tip' => true,
-				'description' => __('Creates order for refused transactions when Pagar.me Checkout is enabled')
+				'title'       => __( 'Refused Transactions Order', 'woocommerce-pagarme' ),
+				'type'        => 'checkbox',
+				'label'       => __( 'Create order for refused transactions', 'woocommerce-pagarme' ),
+				'default'     => 'no',
+				'desc_tip'    => true,
+				'description' => __( 'Creates order for refused transactions when Pagar.me Checkout is enabled' ),
 			),
 			'installments' => array(
 				'title'       => __( 'Installments', 'woocommerce-pagarme' ),
@@ -249,7 +249,7 @@ class WC_Pagarme_Credit_Card_Gateway extends WC_Payment_Gateway {
 						'customerFields'   => $customer,
 						'checkoutPayPage'  => ! empty( $customer ),
 						'uiColor'          => apply_filters( 'wc_pagarme_checkout_ui_color', '#1a6ee1' ),
-						'refused_order'		 => $this->refused_order
+						'refused_order'    => $this->refused_order,
 					)
 				);
 			} else {

claudiosanches/woocommerce-pagarme

assets/js/checkout.js
@@ -212,7 +212,7 @@
 				success: function( data ) {
 					formSubmit = true;
 
-					if( wcPagarmeParams.register_refused_order === "no" ) {
+					if ( 'no' === wcPagarmeParams.register_refused_order ) {
 						// Remove any old token input.
 						$( 'input[name=pagarme_checkout_token]', form ).remove();
 
@@ -239,7 +239,7 @@
 				customer = getCustomerFields( form );
 			}
 
-			var shouldCreateToken = wcPagarmeParams.register_refused_order === "yes"? "false" : "true";
+			const shouldCreateToken = 'yes' === wcPagarmeParams.register_refused_order ? 'false' : 'true';
 
 			// Set params.
 			params = $.extend({}, {

includes/class-wc-pagarme-api.php
@@ -661,10 +661,10 @@ class WC_Pagarme_API {
 	public function process_regular_payment( $order_id ) {
 		$order = wc_get_order( $order_id );
 
-		$isUsingPagarmeCheckout = (isset($this->gateway->checkout) && 'yes' === $this->gateway->checkout);
-		$shouldRegisterDeniedTransaction = (isset($this->gateway->register_refused_order) && 'yes' === $this->gateway->register_refused_order);
+		$is_checkout_pagarme    = isset( $this->gateway->checkout ) && 'yes' === $this->gateway->checkout;
+		$register_refused_order = isset( $this->gateway->register_refused_order ) && 'yes' === $this->gateway->register_refused_order;
 
-		if ( $isUsingPagarmeCheckout && !$shouldRegisterDeniedTransaction ) {
+		if ( $is_checkout_pagarme && ! $register_refused_order ) {
 			if ( ! empty( $_POST['pagarme_checkout_token'] ) ) {
 				$token = sanitize_text_field( wp_unslash( $_POST['pagarme_checkout_token'] ) );
 				$data  = $this->generate_checkout_data( $order, $token );

includes/class-wc-pagarme-credit-card-gateway.php
@@ -34,17 +34,17 @@ class WC_Pagarme_Credit_Card_Gateway extends WC_Payment_Gateway {
 		$this->init_settings();
 
 		// Define user set variables.
-		$this->title                = $this->get_option( 'title' );
-		$this->description          = $this->get_option( 'description' );
-		$this->api_key              = $this->get_option( 'api_key' );
-		$this->encryption_key       = $this->get_option( 'encryption_key' );
-		$this->checkout             = $this->get_option( 'checkout' );
+		$this->title                  = $this->get_option( 'title' );
+		$this->description            = $this->get_option( 'description' );
+		$this->api_key                = $this->get_option( 'api_key' );
+		$this->encryption_key         = $this->get_option( 'encryption_key' );
+		$this->checkout               = $this->get_option( 'checkout' );
 		$this->register_refused_order = $this->get_option( 'register_refused_order' );
-		$this->max_installment      = $this->get_option( 'max_installment' );
-		$this->smallest_installment = $this->get_option( 'smallest_installment' );
-		$this->interest_rate        = $this->get_option( 'interest_rate', '0' );
-		$this->free_installments    = $this->get_option( 'free_installments', '1' );
-		$this->debug                = $this->get_option( 'debug' );
+		$this->max_installment        = $this->get_option( 'max_installment' );
+		$this->smallest_installment   = $this->get_option( 'smallest_installment' );
+		$this->interest_rate          = $this->get_option( 'interest_rate', '0' );
+		$this->free_installments      = $this->get_option( 'free_installments', '1' );
+		$this->debug                  = $this->get_option( 'debug' );
 
 		// Active logs.
 		if ( 'yes' === $this->debug ) {
@@ -135,12 +135,12 @@ class WC_Pagarme_Credit_Card_Gateway extends WC_Payment_Gateway {
 				'description' => __( "When enabled opens a Pagar.me modal window to receive the customer's credit card information.", 'woocommerce-pagarme' ),
 			),
 			'register_refused_order' => array(
-				'title' => __( 'Refused Transactions Order', 'woocommerce-pagarme'),
-				'type' => 'checkbox',
-				'label' => __( 'Create order for refused transactions', 'woocommerce-pagarme'),
-				'default' => 'no',
-				'desc_tip' => true,
-				'description' => __( 'Creates order for refused transactions when Pagar.me Checkout is enabled'),
+				'title'       => __( 'Register Refused Order', 'woocommerce-pagarme' ),
+				'type'        => 'checkbox',
+				'label'       => __( 'Register order for refused transactions', 'woocommerce-pagarme' ),
+				'default'     => 'no',
+				'desc_tip'    => true,
+				'description' => __( 'Register order for refused transactions when Pagar.me Checkout is enabled' ),
 			),
 			'installments' => array(
 				'title'       => __( 'Installments', 'woocommerce-pagarme' ),
@@ -242,14 +242,14 @@ class WC_Pagarme_Credit_Card_Gateway extends WC_Payment_Gateway {
 					'pagarme-checkout',
 					'wcPagarmeParams',
 					array(
-						'encryptionKey'    => $this->encryption_key,
-						'interestRate'     => $this->api->get_interest_rate(),
-						'freeInstallments' => $this->free_installments,
-						'postbackUrl'      => WC()->api_request_url( get_class( $this ) ),
-						'customerFields'   => $customer,
-						'checkoutPayPage'  => ! empty( $customer ),
-						'uiColor'          => apply_filters( 'wc_pagarme_checkout_ui_color', '#1a6ee1' ),
-						'register_refused_order'		 => $this->register_refused_order,
+						'encryptionKey'          => $this->encryption_key,
+						'interestRate'           => $this->api->get_interest_rate(),
+						'freeInstallments'       => $this->free_installments,
+						'postbackUrl'            => WC()->api_request_url( get_class( $this ) ),
+						'customerFields'         => $customer,
+						'checkoutPayPage'        => ! empty( $customer ),
+						'uiColor'                => apply_filters( 'wc_pagarme_checkout_ui_color', '#1a6ee1' ),
+						'register_refused_order' => $this->register_refused_order,
 					)
 				);
 			} else {