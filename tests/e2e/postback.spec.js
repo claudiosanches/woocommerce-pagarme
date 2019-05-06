aabef1765e86c461fefa846a5f03d60e7a018eed
Marcia Lima <marcia.lima@pagar.me>
2019-05-06 18:12:29 -0300

    test: update order transaction via postback

--- tests/e2e/postback.spec.js
+++ tests/e2e/postback.spec.js
@@ -1,8 +1,24 @@
-context('Postback', () => {
-  describe('Update purchase with postback', () => {
-    let postback;
+context('Postback last transaction', () => {
+  describe('when you update a purchase with postback', () => {
+    let postback
 
-    it('Get postbacks', () => {
+    before(() => {
+      cy.configureCreditCard({ checkout: false })
+      cy.addProductToCart()
+      cy.goToCheckoutPage()
+      cy.fillCheckoutForm()
+      cy.selectCreditCard()
+      cy.fillCreditCardForm()
+      cy.placeOrder()
+    })
+
+    it('should be at order received page', () => {
+      cy.url({ timeout: 60000 })
+        .should('include', '/finalizar-compra/order-received/')
+      cy.contains('Pedido recebido')
+    })
+
+    it('should contain at least one postback', () => {
       cy.task('pagarmejs:postback')
         .then(postbacks => {
           expect(postbacks).to.not.be.empty
@@ -10,9 +26,21 @@ context('Postback', () => {
         })
     })
 
-    it('Postback is valid', () => {
+    it('Postback URL should equals test domain', () => {
       expect(postback).to.have
         .property('request_url', 'http://woopagarme/wc-api/WC_Pagarme_Credit_Card_Gateway/')
+    })
+
+    it('should update order transaction via postback', () => {
+      cy.request({
+          method: 'POST',
+          url: 'wc-api/WC_Pagarme_Credit_Card_Gateway/',
+          headers: JSON.parse(postback.headers),
+          body: postback.payload
+        })
+        .then((response) => {
+          expect(response.status).to.eq(200)
+        })
     })
   })
 })
