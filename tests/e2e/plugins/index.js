pagarme/woocommerce-pagarme-archived

test: validate postback refused transaction checkout Pagar.me

tests/e2e/plugins/index.js
@@ -22,6 +22,17 @@ const getClient = (apiKey) => {
   return client
 }
 
+const getTransaction = (apiKey, body) =>
+  getClient(apiKey)
+    .then(client => client.transactions.find(body))
+    .then(transactions => transactions[0])
+
+const getPostback = (apiKey, transactionId) =>
+  getClient(apiKey)
+    .then((client) =>
+      client.postbacks.find({ transactionId: transactionId})
+    )
+
 const getLastTransaction = (apiKey) =>
   getClient(apiKey)
     .then(client => client.transactions.all({ count: 1 }))
@@ -40,7 +51,9 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 
   on('task', {
-    'pagarmejs:transaction': () => getLastTransaction(config.env.API_KEY),
-    'pagarmejs:postback': () => getLastTransactionPostback(config.env.API_KEY)
+    'pagarmejs:transaction': (externalId) => getTransaction(config.env.API_KEY, externalId),
+    'pagarmejs:postback': (transactionId) => getPostback(config.env.API_KEY, transactionId),
+    'pagarmejs:lastTransaction': () => getLastTransaction(config.env.API_KEY),
+    'pagarmejs:lastPostback': () => getLastTransactionPostback(config.env.API_KEY)
   })
 }

claudiosanches/woocommerce-pagarme

tests/e2e/plugins/index.js
@@ -22,6 +22,17 @@ const getClient = (apiKey) => {
   return client
 }
 
+const getTransaction = (apiKey, options) =>
+  getClient(apiKey)
+    .then(client => client.transactions.find(options))
+    .then(transactions => transactions[0])
+
+const getPostback = (apiKey, transactionId) =>
+  getClient(apiKey)
+    .then((client) =>
+      client.postbacks.find({ transactionId: transactionId})
+    )
+
 const getLastTransaction = (apiKey) =>
   getClient(apiKey)
     .then(client => client.transactions.all({ count: 1 }))
@@ -40,7 +51,9 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 
   on('task', {
-    'pagarmejs:transaction': () => getLastTransaction(config.env.API_KEY),
-    'pagarmejs:postback': () => getLastTransactionPostback(config.env.API_KEY)
+    'pagarmejs:transaction': (options) => getTransaction(config.env.API_KEY, options),
+    'pagarmejs:postback': (transactionId) => getPostback(config.env.API_KEY, transactionId),
+    'pagarmejs:lastTransaction': () => getLastTransaction(config.env.API_KEY),
+    'pagarmejs:lastPostback': () => getLastTransactionPostback(config.env.API_KEY)
   })
 }